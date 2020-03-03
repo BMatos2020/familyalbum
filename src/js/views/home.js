db7dc6ccd491818ea9cbdf574591ba269857f521
Brenda Matos <brenda.matosux@gmail.com>
2020-03-03 01:14:15 +0000

    family album boilerplate

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,12 +4,15 @@ import "../../styles/home.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
+		<h1>
+			Capture and Share <br />
+			Precious Moments Privately
+		</h1>
+		{/* <p>
 			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
+		</p> */}
+		<a href="#" className="btn btn-light">
+			Join Today!
 		</a>
 	</div>
 );
